43434fc6af59bcf6d9aaa8ef5f53e2e8e96b7141
Md Nafis Iqbal <nafisiqbal.net2002@gmail.com>
2025-06-21 00:21:16 +0600

    handling error from userhandler

--- handlers/userHandler/userHandler.js
+++ handlers/userHandler/userHandler.js
@@ -10,27 +10,57 @@ const UsersCollection = database.collection('users')
 
 users.get('/:email', async (req, res) => {
     const email = req.params.email;
-    const user = await UsersCollection.findOne({email: email});
-    res.send(user);
+    try {
+        const user = await UsersCollection.findOne({ email: email });
+        if(!user){
+            const err = new Error("User not found");
+            err.statusCode = 404;
+            return next(err);
+        }
+        res.send(user);
+    } catch (error) {
+        const err = new Error("Internal server error");
+        err.statusCode = 500;
+        err.error = error;
+        next(err);
+    }
 })
 
 users.post('/', async (req, res) => {
     const userOjbect = req.body;
-    isUserExists = await UsersCollection.findOne({ email: userOjbect?.email })
-    if (isUserExists) {
-        res.send("Login successful")
-    } else {
-        const result = await UsersCollection.insertOne(userOjbect);
-        res.send(result);
-    }
+    try {
+        isUserExists = await UsersCollection.findOne({ email: userOjbect?.email })
+        if (isUserExists) {
+            res.send("Login successful")
+        } else {
+            const result = await UsersCollection.insertOne(userOjbect);
+            res.send(result);
+        }
 
+    } catch (error) {
+        const err = new Error("Internal server error");
+        err.statusCode = 500;
+        err.error = error;
+        next(err);
+    }
 })
 
-users.patch('/update/:email', async (req, res) => {
+users.patch('/update/:email', async (req, res, next) => {
     const userOjbect = req.body;
     const email = req.params.email;
-    const updatedUser = await UsersCollection.updateOne({email: email},{$set: userOjbect});
-    res.send(updatedUser);
+    try {
+        const updatedUser = await UsersCollection.updateOne({ email: email }, { $set: userOjbect });
+
+        if (updatedUser.matchedCount === 0) {
+            const error = new Error("User not found");
+            error.statusCode = 404
+            return next(error);
+        }
+
+        res.send(updatedUser);
+    } catch (error) {
+        next(error);
+    }
 })
 
 
